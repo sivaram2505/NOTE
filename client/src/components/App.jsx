bcf261ef70f386a8990406692ed8ee509d21b73c
sivaram2505 <sivamandalapu25@gmail.com>
2022-07-30 14:47:48 +0530

    deploy

---
+++ client/src/components/App.jsx
@@ -0,0 +1,60 @@
+import "../styles.css";
+import React, { useState , useEffect} from "react";
+import axios from "axios";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+const url = "https://sleepy-hollows-86640.herokuapp.com";
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get(url+"/")
+    .then((res)=>{
+      setNotes(res.data);
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+  },[]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+    axios.post(url+"/add", newNote)
+      .catch((err) => console.log(err));
+  }
+
+  function deleteNote(obj) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== obj.id;
+      });
+    });
+    axios.post(url+"/delete", {title: obj.title, content: obj.content})
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const _ = require("lodash");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const dotenv = require("dotenv");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
+//const db = mongoose.connection;
+//db.once('open', () => console.log("Successfully Connected to Database"));
+
+const noteSchema = {title: String, content: String};
+const note = mongoose.model("note",noteSchema);
+
+app.get("/", function (req,res){
+
+    note.find((err,result) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.json(result);
+
+            console.log("Responded to Get Request on /");
+            // console.log(result);
+        }
+    });
+});
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.post("/add", function(req,res){
+
+    const newNote = new note(req.body);
+    newNote.save();
+
+    console.log("New Note Added Successfully");
+      res.redirect("/");
+    // console.log(req.body);
+});
+
+app.post("/delete", function(req,res){
+
+    const noteTitle = req.body.title;
+    const noteContent = req.body.content;
+
+    note.findOneAndDelete({title: noteTitle, content: noteContent}, (err) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.log("Note Deleted Successfully");
+              res.redirect("/");
+            // console.log(req.body);
+        }
+    });
+});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 4000;
+}
+app.listen(port, function() {
+    console.log("Node Server started on port 4000");
+});
