be8cefba644808c9a255f33173a243b2acf4e4c8
sivaram2505 <79786425+sivaram2505@users.noreply.github.com>
2023-02-17 12:29:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,13 +78,13 @@ app.post("/delete", function(req,res){
         }
     });
 });
-// if (process.env.NODE_ENV === 'production'){
-// app.use(express.static(path.join(__dirname, "/client/build")));
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build/index.html"));
-// });
-// }
+ if (process.env.NODE_ENV === 'production'){
+ app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+}
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 4000;
